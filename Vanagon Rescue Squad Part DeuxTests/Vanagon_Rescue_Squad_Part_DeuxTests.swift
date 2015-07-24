628ab4114c96c69ebe75b0bb559b22b14cdd5210
Barry Allard <barry.allard@gmail.com>
2015-07-24 16:51:28 -0700

    Initial Commit

---
+++ Vanagon Rescue Squad Part DeuxTests/Vanagon_Rescue_Squad_Part_DeuxTests.swift
@@ -0,0 +1,36 @@
+//
+//  Vanagon_Rescue_Squad_Part_DeuxTests.swift
+//  Vanagon Rescue Squad Part DeuxTests
+//
+//  Created by Bad Motherfucker on 7/24/15.
+//  Copyright (c) 2015 BMF. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Vanagon_Rescue_Squad_Part_DeuxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
